76926af68efc12d5018f9e64f8d302752784ff34
jes708 <jonschwarz@nyu.edu>
2016-04-12 17:44:07 -0400

    haiku can read from Franklin

--- haiku.js
+++ haiku.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var cmudictFile = fs.readFileSync('./cmudict.txt');
+var franklinFile = fs.readFileSync('./Franklin.txt');
 
 function formatData(data) {
   
@@ -15,18 +16,23 @@ function formatData(data) {
       syllables = lineSplit[1].match(/\d/g);
       
       if (syllables) {
-        len = syllables.length  
+        len = syllables.length 
+
         if (lineSplit[0].slice(-1) !== ")") {
-          if (wordBySyllables.hasOwnProperty(len)) {
-            wordBySyllables[len].push(lineSplit[0]);
-          } else {
-            wordBySyllables[len] = [lineSplit[0]];
-          }
+          buildObj(wordBySyllables, len, lineSplit[0])
         }
       }
     }
   });
   return wordBySyllables;
+}
+
+function buildObj(obj, prop, val) {
+  if (obj.hasOwnProperty(prop)) {
+    obj[prop].push(val);
+  } else {
+    obj[prop] = [val];
+  }  
 }
 
 function randSumTo(num) {
@@ -46,7 +52,11 @@ function randSumTo(num) {
 
 function chooseWord(syllables) {
   var options = formatData(cmudictFile)[syllables];
-  return options[Math.floor(Math.random() * options.length)];
+  return options[randIndex(options)];
+}
+
+function randIndex(arr) {
+  return Math.floor(Math.random() * arr.length);
 }
 
 function structurer() {
@@ -63,4 +73,42 @@ function createHaiku(structure) {
   }
 }
 
+function haikuSearch(text) {
+  var textArr = text.toString().split(/[\W|_]*\s[\W|_]*/);
+  var i = randIndex(textArr);
+  var dict = formatData(cmudictFile);
+  var lines = [[], [], []];
+  var syllablesRequired = [5, 7, 5]
+
+
+  for (var j = 0; j < 3; j++) {
+    var syllables = 0;
+
+    while (syllables !== syllablesRequired[j]) {
+      for (var prop in dict) {
+        if (dict[prop].indexOf(textArr[i].toUpperCase()) !== -1) {
+          lines[j].push(textArr[i]);
+          var syllablesToAdd = Number(prop);
+          syllables += syllablesToAdd;
+          break;
+        }
+      }
+      i++
+      if (syllables > syllablesRequired[j] || !(textArr[i])) {
+        syllables = 0;
+        lines[j] = [];
+        i = randIndex(textArr);
+      }
+    }
+  }
+
+  var haiku = lines.map(function(line) {
+    return line.join(" ");
+  }).join("\n");
+  console.log(haiku);
+}
+
 console.log(createHaiku(structurer()));
+console.log(haikuSearch(franklinFile));
+
+// As a next step, let the user choose a random haiku, or a specific text, or they can type in numbers.
